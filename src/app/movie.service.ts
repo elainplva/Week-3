ad1eeb78cf4a8318c6b8e41bc00c29b1d9f64b9a
elainplva <s00250500@atu.ie>
2025-02-04 15:11:17 +0000

    service

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -6,4 +6,15 @@ import { Injectable } from '@angular/core';
 export class MovieService {
 
   constructor() { }
+
+  moviesList = [
+    {id: 1, title: 'The GodFather', year: '1972', director: 'Francis Ford Coppola' },
+    {id: 2, title: 'Millers Crossing', year: '1990', director: 'The Coen Brothers' },
+    {id: 3, title: 'Dial M for Murder', year: '1954', director: 'Alfred Hitchcock' },
+    {id: 4, title: 'Whiplash', year: '2014', director: 'Damien Chanzelle ' }
+  ];
+
+  getMovies() {
+    return this.moviesList;
+  }
 }
