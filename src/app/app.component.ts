5c48abc16f420575c6998712761ce36a491dc4e5
elainplva <s00250500@atu.ie>
2025-02-04 15:23:26 +0000

    movie.servive works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { MovieService } from './services/movie.service';
+import { MovieService } from './movie.service';
 
 
 
