0919660667ff6870f8753308970f0705faa00b39
elainplva <s00250500@atu.ie>
2025-02-04 14:46:09 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,12 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'movie-list';
+  title = 'movie-list - Elain Polakova s00250500';
+
+  moviesList = [
+    {id: 1, title: 'The GodFather', year: '1972', director: 'Francis Ford Coppola' },
+    {id: 2, title: 'Millers Crossing', year: '1990', director: 'The Coen Brothers' },
+    {id: 3, title: 'Dial M for Murder', year: '1954', director: 'Alfred Hitchcock' },
+    {id: 4, title: 'Whiplash', year: '2014', director: 'Damien Chanzelle ' }
+  ];
 }
