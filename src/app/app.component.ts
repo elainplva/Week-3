b2eb9b295c26b1f6aa90e7ad382b5f744e31ca9d
elainplva <s00250500@atu.ie>
2025-02-04 15:21:40 +0000

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MovieService } from './services/movie.service';
+
 
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
@@ -13,10 +14,13 @@ import { CommonModule } from '@angular/common';
 export class AppComponent {
   title = 'movie-list - Elain Polakova s00250500';
 
-  moviesList = [
-    {id: 1, title: 'The GodFather', year: '1972', director: 'Francis Ford Coppola' },
-    {id: 2, title: 'Millers Crossing', year: '1990', director: 'The Coen Brothers' },
-    {id: 3, title: 'Dial M for Murder', year: '1954', director: 'Alfred Hitchcock' },
-    {id: 4, title: 'Whiplash', year: '2014', director: 'Damien Chanzelle ' }
-  ];
+  movies:any[]=[];
+
+  constructor(private movieService:MovieService) {}
+
+  ngOnInit() {
+    this.movies=this.movieService.getMovies();
+    console.log(this.movies);
+  }
+ 
 }
