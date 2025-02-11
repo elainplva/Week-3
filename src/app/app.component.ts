d8bcc6b4093ae77211c380c944a4d555b77fdbd1
elainplva <s00250500@atu.ie>
2025-02-11 16:17:50 +0000

    name and screenshot added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { MovieService } from './movie.service';
 
 import { AddMovieComponent } from './add-movie/add-movie.component';
 import { ListMoviesComponent } from './list-movies/list-movies.component';
@@ -10,12 +8,13 @@ import { ListMoviesComponent } from './list-movies/list-movies.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, AddMovieComponent, ListMoviesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'movie-list - Elain Polakova s00250500';
+  title = 'movie-list';
 
  
   
