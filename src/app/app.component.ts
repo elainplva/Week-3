e7c4018cb37329f99391b13fee242b7030b5f29e
elainplva <s00250500@atu.ie>
2025-02-18 15:46:36 +0000

    final step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Movie } from './movie.model';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+
 
 import { AddMovieComponent } from './add-movie/add-movie.component';
 import { ListMoviesComponent } from './list-movies/list-movies.component';
@@ -9,12 +12,18 @@ import { ListMoviesComponent } from './list-movies/list-movies.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AddMovieComponent, ListMoviesComponent],
+  imports: [RouterOutlet, AddMovieComponent, ListMoviesComponent, MovieDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'movie-list';
+
+  public mySelectedMovie!:Movie;
+
+  setSelectedMovie(movie:Movie) {
+    this.mySelectedMovie=movie;
+  }
 
  
   
--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { Movie } from '../movie.model';
 
 @Component({
   selector: 'app-movie-detail',
+  standalone: true,
   imports: [],
   templateUrl: './movie-detail.component.html',
   styleUrl: './movie-detail.component.css'
 })
 export class MovieDetailComponent {
-
+ @Input() movieDetails!:Movie;
 }
